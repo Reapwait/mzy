d3deeb3da43c3e9ee61f9cb89188db711b21fbcb
Reapwait <98075862+Reapwait@users.noreply.github.com>
2022-03-12 14:30:50 +0800

    Add files via upload

---
+++ 两数相乘基础练习题.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args){
+        double a,b;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextDouble();
+        b = scan.nextDouble();
+        System.out.println(a * b);
+    }
+}
