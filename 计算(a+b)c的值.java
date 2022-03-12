a7ee1eaaeff93c1c1836d0ab2d9063243edadcaa
Reapwait <98075862+Reapwait@users.noreply.github.com>
2022-03-12 14:49:01 +0800

    Add files via upload

---
+++ 计算(a+b)c的值.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args){
+        double a,b,c;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextDouble();
+        b = scan.nextDouble();
+        c = scan .nextDouble();
+        System.out.println((a+b)*c);
+    }
+}
