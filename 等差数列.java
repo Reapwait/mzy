7c6271bab82444b3eb3410dd5cfde11f92a058b6
Reapwait <98075862+Reapwait@users.noreply.github.com>
2022-03-12 14:46:40 +0800

    Add files via upload

---
+++ 等差数列.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args){
+        double a,b,n;
+        double ne;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextDouble();
+        b = scan.nextDouble();
+        n = scan .nextDouble();
+        System.out.println((a+(n-1) * (b-a)));
+
+    }
+}
